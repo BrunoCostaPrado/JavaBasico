0e2de948286b5864bf37eab8128419b440a42bf9
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-22 12:22:21 -0300

    .

--- src/exercicio/CustoFinal.java
+++ src/exercicio/CustoFinal.java
@@ -6,9 +6,9 @@ public class CustoFinal {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Insira o custo de fabrica: ");
-		int c = input.nextInt();
-		int p = 100 * c;
-		int cf = (c + (28 / p) + (45 / p));
+		float c = input.nextFloat();
+		float p = c * 100;
+		float cf = ((c * (28 / p)) + (c * (45 / p)) + c);
 		System.out.print("O custo final é: " + cf);
 		input.close();
 	}
