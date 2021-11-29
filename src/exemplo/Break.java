935c460480b63601b165724b98dc73946a502e98
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-11-29 12:38:47 -0300

    .

---
+++ src/exemplo/Break.java
@@ -0,0 +1,16 @@
+package exemplo;
+
+public class Break {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {
+
+			if (i == 6) {
+				break;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
--- src/exercicio/RealDolar.java
+++ src/exercicio/RealDolar.java
@@ -2,17 +2,17 @@ package exercicio;
 
 import java.util.Scanner;
 
-public class DolarReal {
+public class RealDolar {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		float a, b, c, d;
+		float a, b, c;
 		System.out.println("Insira a cotação do dolar");
 		a = input.nextFloat();
-		System.out.println("Insira a quantidade a ser convertida");
+		System.out.println("Insira o valor em real");
 		b = input.nextFloat();
 		c = a * b;
-		System.out.println("O valor em reais é: " + c + " reais");
+		System.out.println("O valor em dolar é: " + c + " dolares");
 		input.close();
 
 	}
