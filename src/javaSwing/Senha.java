c996fa243fec9d966fe771756bf636afaac2b595
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-12-03 09:53:10 -0300

    ,

--- src/javaSwing/Senha.java
+++ src/javaSwing/Senha.java
@@ -3,18 +3,16 @@ package javaSwing;
 import javax.swing.*;
 
 public class Senha {
-
 	public static void main(String[] args) {
 		JFrame f = new JFrame("Senha");
-		JPasswordField v = new JPasswordField();
-		JLabel l1 = new JLabel("Senha");
+		JPasswordField value = new JPasswordField();
+		JLabel l1 = new JLabel("Senha:");
 		l1.setBounds(20, 100, 80, 30);
-			value.setBounds(100, 100, 100, 30);
+		value.setBounds(100, 100, 100, 30);
 		f.add(value);
 		f.add(l1);
 		f.setSize(300, 300);
 		f.setLayout(null);
 		f.setVisible(true);
 	}
-
 }
---
+++ src/javaSwing/VerURL.java
@@ -0,0 +1,61 @@
+package javaSwing;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.InputStream;
+import java.net.*;
+
+public class VerURL extends Frame implements ActionListener {
+	TextField tf;
+	TextArea ta;
+	Button b;
+	Label l;
+
+	VerURL() {
+		super("Ferramenta de URL");
+		l = new Label("Insira URL");
+		l.setBounds(50, 50, 50, 20);
+		tf = new TextField();
+		tf.setBounds(120, 50, 250, 20);
+		b = new Button("Insira a URL");
+		b.setBounds(120, 100, 120, 30);
+		b.addActionListener(this);
+		ta = new TextArea();
+		ta.setBounds(120, 150, 250, 150);
+
+		add(l);
+		add(tf);
+		add(b);
+		add(ta);
+		setSize(400, 400);
+		setLayout(null);
+		setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		String s = tf.getText();
+		if (s == null) {
+		} else {
+			try {
+				URL u = new URL(s);
+				URLConnection uc = u.openConnection();
+				InputStream is = uc.getInputStream();
+				int i;
+				StringBuilder sb = new StringBuilder();
+				while ((i = is.read()) != -1) {
+					sb.append((char) i);
+				}
+				String source = sb.toString();
+				ta.setText(source);
+			} catch (Exception ex) {
+				System.out.println(e);
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		new VerURL();
+	}
+
+}
