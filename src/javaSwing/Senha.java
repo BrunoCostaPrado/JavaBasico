5f069a264463b47259e7b747d2419666747be41c
BrunoCostaPrado <brunocostaprado@gmail.com>
2021-12-02 22:58:01 -0300

    ,

---
+++ src/javaSwing/Senha.java
@@ -0,0 +1,20 @@
+package javaSwing;
+
+import javax.swing.*;
+
+public class Senha {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("Senha");
+		JPasswordField v = new JPasswordField();
+		JLabel l1 = new JLabel("Senha");
+		l1.setBounds(20, 100, 80, 30);
+			value.setBounds(100, 100, 100, 30);
+		f.add(value);
+		f.add(l1);
+		f.setSize(300, 300);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+
+}
